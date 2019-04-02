cd10e0af9d42cf97cfc01c5c2003316f9b00efcb
Dario Elbadry <elbadry.d@gmail.com>
2019-04-02 14:17:57 -0500

    fetching book data on client

--- server/app.js
+++ server/app.js
@@ -1,38 +1,10 @@
-// const express = require('express');
-// const app = express();
-// const graphqlHTTP = require('express-graphql');
-// const schema = require('./schema/schema');
-
-// const mongoose = require('mongoose');
-// const DB_NAME = "gql-tutorial"
-// const MongoClient = require('mongodb').MongoClient;
-// let database;
-
-
-// // bind express with graphql
-// app.use('/graphql', graphqlHTTP({
-//     schema,
-//     graphiql: true
-// }));
-
-// app.listen(4000, () => {
-//     console.log('now listening for requests on port 4000');
-//     MongoClient.connect(uri, { useNewUrlParser: true }, (error, client) => {
-//         if(error) {
-//             console.log(error)
-//         } else {
-//             database = client.db(DATABASE_NAME);
-//             collection = database.collection("library");
-//             console.log("Connected to `" + DB_NAME + "`!");
-//         }
-//         })
-// });
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv').config()
 const uri = `mongodb+srv://elbadryd:${process.env.MONGO_PW}@cluster0-mclid.mongodb.net/gql-tutorial`
+const cors = require('cors');
 
 const app = express();
 
@@ -42,7 +14,7 @@ mongoose.connect(uri, { useNewUrlParser: true })
 mongoose.connection.once('open', () => {
     console.log('conneted to database');
 });
-
+app.use(cors())
 // bind express with graphql
 app.use('/graphql', graphqlHTTP({
     schema,
